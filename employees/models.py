5ea6ca775b12a61fd303f37c6378f7a12496848b
JPAKIRA <joaopauloribeiromyszka@gmail.com>
2022-01-25 22:13:02 -0300

    add models and DRF to project

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    name = models.CharField(verbose_name="Name", max_length=255)
+    email = models.EmailField(max_length=255)
+    department = models.CharField(verbose_name="Department", max_length=50)
+
